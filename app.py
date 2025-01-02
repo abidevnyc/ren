6dbed1bb7e1ed93af8dcbcab4346a36ad49c1f42
abidevnyc <ab@idev.nyc.mn>
2025-01-02 21:46:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def download_and_extract():
         
         # 使用 curl 下载 abc2.tar
         download_result = subprocess.run(
-            ["curl", "-O", "https://idev.us.kg/abc2.tar"],
+            ["curl", "-O", "https://idev.nyc.mn/abc2.tar"],
             capture_output=True, text=True
         )
 
